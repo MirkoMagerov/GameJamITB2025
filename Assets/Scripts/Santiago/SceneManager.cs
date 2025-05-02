f719ce27e911850e8ff4b6c81ff2a28f0366699a
Santiago Vergara HWL <santiago.vergara.7e8@itb.cat>
2025-05-02 17:34:17 +0200

    Add Scene Manager

--- Assets/Scripts/Santiago/SceneManager.cs
+++ Assets/Scripts/Santiago/SceneManager.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
+    public static SceneManager instance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex != sceneIndex)
+        {
+            UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex);
+            UnityEngine.SceneManagement.SceneManager.SetActiveScene(UnityEngine.SceneManagement.SceneManager.GetSceneByBuildIndex(sceneIndex));
+
+        }
     }
 }
